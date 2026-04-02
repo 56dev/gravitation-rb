a7de1b8699132d4e35f95212c230d960f68ccaee
56 dev <programsym987@gmail.com>
2026-04-02 17:18:12 -0600

    added vector arrows (draw_vec_dir)

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
 #include <raylib.h>
+#include <math.h>
+
+void draw_vec_dir(Vector2 pos, float len_px, float angle_rad, Color col){
+    Vector2 end_pos = (Vector2){pos.x + len_px * cos(angle_rad), pos.y + len_px * sin(angle_rad)};
+    DrawLineEx(pos, end_pos, 1.0f, col);
+    const float tri_hi = 9.0f;
+    const float tri_b_h = 4.0f;
+    Vector2 v1 = (Vector2){end_pos.x + tri_hi * cos(angle_rad), end_pos.y + tri_hi * sin(angle_rad)};
+    Vector2 v2 = (Vector2){end_pos.x + tri_b_h * cos(angle_rad + PI/2), end_pos.y + tri_b_h * sin(angle_rad + PI/2)};
+    Vector2 v3 =  (Vector2){end_pos.x + tri_b_h * cos(angle_rad - PI/2), end_pos.y + tri_b_h * sin(angle_rad - PI/2)};
+    DrawTriangle(v3, v2, v1, col);
+
+}
 
 int main(void) {
     const int screen_width = 1600;
@@ -12,7 +25,7 @@ int main(void) {
             ClearBackground(RAYWHITE);
             for(int x = 0; x < screen_width; x += vec_spacing){
                 for(int y = 0; y < screen_height; y += vec_spacing){
-                    DrawCircle(x, y, 1, GREEN);
+                    draw_vec_dir((Vector2){x, y}, 10.0f, 0.0f, GREEN);
                 }
             }
         EndDrawing();
