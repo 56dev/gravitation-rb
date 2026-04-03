23fe7e7d96fd2c1641ea1ac459b1f79868c8b60c
56 dev <programsym987@gmail.com>
2026-04-03 16:30:59 -0600

    added a draw_arrowhead function to split up some of the functionality of the draw_vec functions

--- main.c
+++ main.c
@@ -8,17 +8,19 @@ typedef struct obj_s {
     Vector2 vel_px_s;
 } obj_s;
 
+void draw_arrowhead(float angle_rad, float tri_hi, float tri_b_h, Vector2 apex_pos, Color col){
+    Vector2 d = (Vector2){apex_pos.x - tri_hi * cos(angle_rad), apex_pos.y - tri_hi * sin(angle_rad)};
+    Vector2 v2 = (Vector2){d.x + tri_b_h * cos(angle_rad + PI/2), d.y + tri_b_h * sin(angle_rad + PI/2)};
+    Vector2 v3 =  (Vector2){d.x + tri_b_h * cos(angle_rad - PI/2), d.y + tri_b_h * sin(angle_rad - PI/2)};
+    DrawTriangle(v3, v2, apex_pos, col);
+}
 
 void draw_vec_dir(Vector2 pos, float len_px, float angle_rad, Color col){
     Vector2 end_pos = (Vector2){pos.x + len_px * cos(angle_rad), pos.y + len_px * sin(angle_rad)};
     DrawLineEx(pos, end_pos, 1.0f, col);
     const float tri_hi = 9.0f;
     const float tri_b_h = 2.0f;
-    Vector2 v1 = (Vector2){end_pos.x + tri_hi * cos(angle_rad), end_pos.y + tri_hi * sin(angle_rad)};
-    Vector2 v2 = (Vector2){end_pos.x + tri_b_h * cos(angle_rad + PI/2), end_pos.y + tri_b_h * sin(angle_rad + PI/2)};
-    Vector2 v3 =  (Vector2){end_pos.x + tri_b_h * cos(angle_rad - PI/2), end_pos.y + tri_b_h * sin(angle_rad - PI/2)};
-    DrawTriangle(v3, v2, v1, col);
-
+    draw_arrowhead(angle_rad, tri_hi, tri_b_h, end_pos, col);
 }
 
 void draw_vec_end(Vector2 pos, Vector2 to_end, Color col){
@@ -31,12 +33,10 @@ void draw_vec_end(Vector2 pos, Vector2 to_end, Color col){
     float thet = atan2f(to_end.y, to_end.x);
     const float tri_hi = 9.0f;
     const float tri_b_h = 4.0f; 
-    Vector2 v1 = (Vector2){end.x + tri_hi * cos(thet), end.y + tri_hi * sin(thet)};
-    Vector2 v2 = (Vector2){end.x + tri_b_h * cos(thet + PI/2), end.y + tri_b_h * sin(thet + PI/2)};
-    Vector2 v3 =  (Vector2){end.x + tri_b_h * cos(thet - PI/2), end.y + tri_b_h * sin(thet - PI/2)};
-    DrawTriangle(v3, v2, v1, col);
-
+    draw_arrowhead(thet, tri_hi, tri_b_h, end, col);
 }
+
+
 
 Vector2 calc_g_field_at_point_ignore_one_obj(Vector2 point, obj_s *obj_a, int obj_n, int idx_ignore){
     Vector2 ret = (Vector2){0, 0};
@@ -76,8 +76,8 @@ int main(void) {
     const int vec_spacing = 15;
     #define num_obj 2
     obj_s objects[num_obj] = {
-        (obj_s){100.0f, (Vector2){screen_width/2, screen_height/2}, (Vector2){0, 0}},
-        (obj_s){300.0f, (Vector2){screen_width/4.0f, screen_height/2}, (Vector2){0, 0}}
+        (obj_s){100.0f, (Vector2){screen_width/2, screen_height/2}, (Vector2){0,35.0f}},
+        (obj_s){300.0f, (Vector2){screen_width/4.0f, screen_height/2}, (Vector2){20.0f, 0}}
         };
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -96,6 +96,7 @@ int main(void) {
                     col.b = (mag / MAX_MAG) * 255;
                     col.g = ((MAX_MAG - mag) / MAX_MAG) * 255;
                     draw_vec_end((Vector2){x,y}, disp, col);
+                    
 //                    DrawCircle(x, y, 2.0f, ORANGE);
                 }
             }
